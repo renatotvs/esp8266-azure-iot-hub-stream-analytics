a0e97d4520715b13823aa75c267fbae9e2cb94a9
Renato Tavares <42357180+renatotvs@users.noreply.github.com>
2022-03-29 16:50:38 -0300

    Update config.h

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define DEVICE_ID "NodeMCU" //Device ID cadastrado no Azure IoT Hub
 #define my_SSID "" //Nome da rede wifi
 #define my_PWD "" //Senha da rede wifi
-#define DeviceConnStr "" //connection string do device configurado no Azure IoT Hub
+#define DeviceConnStr "" //Primary Connection String do device configurado no Azure IoT Hub
 
 #define VALUE_ALERT 30
 #define INTERVAL 3000 //tempo em milissegundos
